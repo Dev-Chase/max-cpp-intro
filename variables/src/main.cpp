3d9720cdd27d3ca79996dd20526c93e4d995332a
Dev-Chase <chasecurwin@gmail.com>
2024-01-18 11:03:32 -0500

    Variables

---
+++ variables/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int a = 0;
+  int b = 2;
+  char c = 'c';
+  unsigned x = 8;
+
+  char exit[] = "EXIT";
+  string testing = "TESTING";
+
+  bool like = false;
+
+  return 0;
+}
